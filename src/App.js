815528d023344896806a044729b8f3cb0b05bb82
tomonakar <tomohisa.nak@gmail.com>
2019-08-10 12:19:41 +0900

    resetメソッドを削除して直接リセット処理をonClickで実行 - reset関数が短くなったのでメソッドを削除した

--- src/App.js
+++ src/App.js
@@ -4,18 +4,13 @@ const App = props => {
   // useStateはオブジェクトを受け取って初期化することができる
   const [state, setState] = useState(props)
 
-  // ステートの更新もオブジェクトを使って一度に定義できる
-  const reset = () => {
-    setState(props)
-  }
-
 // エレメントの中で使う関数も以下のように書き換える
   return (
     <>
       <p>{state.name}の値段は{state.price}円</p>
       <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
       <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
-      <button onClick={reset}>reset</button>
+      <button onClick={() => setState(props)}>reset</button>
       <input value={state.name} onChange={e => setState({...state, name: e.target.value})} />
     </>
   )
