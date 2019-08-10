aa2fc9154585dedc3f65b6b9dee3bd021610be19
tomonakar <tomohisa.nak@gmail.com>
2019-08-10 15:55:31 +0900

    feat: useReducerを使ったステート更新処理

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,48 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
 
 // bootstrapを適用
 // @see: https://getbootstrap.com/docs/4.3/getting-started/webpack/#importing-compiled-css
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import reducer from '../reducers'
+
 const App = props => {
+  // useReducerの使い方 第3引数は初期化時にやりたい処理を渡す
+  // const [state, dispatch] = useReducer(reducer, initialState, init)
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addEvent = e => {
+    e.preventDefault()
+    const action = {
+      type: 'CREATE_EVENT',
+      title,
+      body
+    }
+    dispatch(action)
+    setTitle('')
+    setBody('')
+  }
+
+  console.log({state})
+
   return (
     <div className='container-fluid'>
       <h4>イベント作成フォーム</h4>
       <form>
         <div className='form-group'>
           <label htmlFor='formEventTitle'>タイトル</label>
-          <input className='form-control' id='formEventTitle' />
+          <input className='form-control' id='formEventTitle' value={title} onChange={e => setTitle(e.target.value)}/>
         </div>
 
         <div className='form-group'>
           <label htmlFor='formEventBody'>ボディ</label>
-          <textarea className='form-control' id='formEventBody' />
+          <textarea className='form-control' id='formEventBody' value={body} onChange={e => setBody(e.target.value)} />
         </div>
 
-        <button class='btn btn-primary'>イベント作成</button>
-        <button class='btn btn-danger'>全イベント削除</button>
+        <button className='btn btn-primary' onClick={addEvent}>イベント作成</button>
+        <button className='btn btn-danger'>全イベント削除</button>
 
         <h4>イベント一覧</h4>
         <table className='table table-hover'>
