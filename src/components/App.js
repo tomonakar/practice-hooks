0dfa5dda410bb5a1400ab2c7a40357437e2fd4dd
tomonakar <tomohisa.nak@gmail.com>
2019-08-12 14:28:05 +0900

    refactor: contextを利用してprop dorilling問題を解消（propsのバケツリレーを解消)

--- src/components/App.js
+++ src/components/App.js
@@ -9,17 +9,16 @@ import EventForm from './EventForm'
 import reducer from '../reducers'
 import AppContext from '../contexts/AppContext'
 
-console.log({AppContext})
 const App = () => {
   // useReducerの使い方 第3引数は初期化時にやりたい処理を渡す
   // const [state, dispatch] = useReducer(reducer, initialState, init)
   const [state, dispatch] = useReducer(reducer, [])
 
   return (
-    <AppContext.Provider value={'Hello, I am a provider'}>
+    <AppContext.Provider value={{ state, dispatch }}>
       <div className='container-fluid'>
-        <EventForm  state={state} dispatch={dispatch} />
-        <Events state={state} dispatch={dispatch} />
+        <EventForm />
+        <Events />
       </div>
     </AppContext.Provider>
   )
