07bf2792a683de3983b466407c0d20d2368634ff
tomonakar <tomohisa.nak@gmail.com>
2019-08-12 13:22:37 +0900

    feat: useContextを使ってcontextProviderを利用

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import Event from './Event'
+import AppContext from '../contexts/AppContext'
+
 const Events = ({ dispatch, state }) => {
+  const value = useContext(AppContext)
+
   return (
     <React.Fragment>
-      <h4>イベント一覧</h4>
-      <table className='table table-hover'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Body</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            state.map((event, index) => {
-              return <Event key={index} event={event} dispatch={dispatch} />
-            })
-          }
-        </tbody>
-      </table>
+        <div>{value}</div>
+        <h4>イベント一覧</h4>
+        <table className='table table-hover'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              state.map((event, index) => {
+                return <Event key={index} event={event} dispatch={dispatch} />
+              })
+            }
+          </tbody>
+        </table>
     </React.Fragment>
   )
 }
